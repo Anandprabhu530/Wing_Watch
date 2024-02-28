c5a7860ad9c2d889435ac60d78f72697800997f1
Anand Prabhu <97842562+Anandprabhu530@users.noreply.github.com>
2024-02-28 11:45:40 +0000

    UI Changes

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,15 +1,14 @@
+import { sampledata } from "../_components/data"
+
 const Profile = () =>{
     return(
         <div className="border border-white w-full h-screen p-10">
             <div className="flex flex-col gap-4">
             <div className="font-semibold text-xl">UserName here</div>
-            <div>Total posts : 34</div>
-            <div className="grid grid-cols-2 gap-4">
-                <div className="border border-white">Hello World</div>
-                <div className="border border-white">Hello World</div>
-                <div className="border border-white">Hello World</div>
-                <div className="border border-white">Hello World</div>
-                <div className="border border-white">Hello World</div>
+            <div>Total posts : {sampledata.length}</div>
+            <div className="grid grid-cols-2 gap-4">{sampledata.map((solodata)=>{return(
+                <img className="object-cover border border-white" src={solodata.ImageUrl} key={solodata.ImageUrl}/>
+            )})}
             </div>
             </div>
         </div>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,5 @@
+import { sampledata } from "../_components/data";
+
 const Home = () => {
   return (
     
@@ -34,60 +36,4 @@ const Home = () => {
 };
 
 
-const sampledata = [{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-},
-{
-  BirdName:"Eagle",
-  Location:"India",
-  ImageUrl:"https://images.unsplash.com/photo-1444464666168-49d633b86797?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  CreatedBY:"Anand",
-  Wings:342,
-  Description:"A small Eagle photo from a riverside"
-}]
 export default Home;
