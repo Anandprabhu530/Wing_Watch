f72b76beffcefa277f52d8c1e780d0a52434c92f
Anand Prabhu <97842562+Anandprabhu530@users.noreply.github.com>
2024-02-29 14:48:27 +0000

    Ui changes

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -2,20 +2,24 @@ import Link from 'next/link'
 
 const Homelayout = ({ children }) => {
   return (
-    <div className="flex">
-      <div className="w-[50%] mx-auto h-screen flex">
-        <div className="basis-1/3 overflow-hidden">
-          <div className="flex flex-col pt-10 items-center gap-6">
-            <Link href="/home">Home</Link>
-            <Link href="/id">Profile</Link>
-            <Link href="/id/settings">Settings</Link>
-            <Link href="/home/post">New Post</Link>
+    <div className="flex w-full h-screen">
+      <div className='basis-2/6'></div>
+      <div className='basis-3/6'>
+        <div className='flex w-full'>
+          <div className="basis-1/3">
+            <div className="flex flex-col pt-10 items-center gap-6 fixed pl-16">
+              <Link href="/home">Home</Link>
+              <Link href="/id">Profile</Link>
+              <Link href="/id/settings">Settings</Link>
+              <Link href="/home/post">New Post</Link>
+            </div>
+          </div>
+          <div className="basis-2/3 overflow-y-auto">
+            <div>{children}</div>
           </div>
         </div>
-        <div className="basis-2/3 overflow-auto">
-          <div>{children}</div>
-        </div>
       </div>
+      <div className='basis-2/6'></div>  
     </div>
   );
 };
