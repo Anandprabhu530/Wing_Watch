4107f30c49d495ff30abaa2836b33330c21fd341
Anand Prabhu <97842562+Anandprabhu530@users.noreply.github.com>
2024-03-05 10:07:20 +0000

    Added Login and register backend

--- server/main.go
+++ server/main.go
@@ -1,14 +1,25 @@
 package main 
 
 import(
+	"os"
     "fmt"
+	"time"
+	"net/http"
     // "log"
-    "os"
-	"gorm.io/driver/postgres"
+
 	"gorm.io/gorm"
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/driver/postgres"
     "github.com/joho/godotenv"
     "github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique"`
+	Password string
+}
 
 var DB *gorm.DB
 
@@ -18,23 +29,87 @@ func init(){
 		fmt.Println("Error loading .env file")
 	}
 	var dberr error
+
 	dsn := os.Getenv("connStr")
+	
 	DB, dberr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if(dberr!=nil){
 		fmt.Println(dberr)
 	}
-    type User struct {
-		gorm.Model
-		Name string
-	}
+	
+	DB.AutoMigrate(&User{})
 }
 
 func main(){
     r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/register", register)
+	r.POST("/login",login)
 	r.Run()
+}
+
+func login(c *gin.Context){
+	var body struct {
+		Username string
+		Password string
+	}
+	if c.Bind(&body) != nil{
+		fmt.Println("Cannot bind the data");
+		return
+	}
+	
+	var user User
+	DB.First(&user, "Username = ?", body.Username)
+	if user.ID == 0{
+		fmt.Println("Username or password is incorrect");
+		return;
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(body.Password))
+	if err!=nil{
+		fmt.Println("Error in Encryption")
+		return;
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+	
+	tokenString, err := token.SignedString([]byte(os.Getenv("hashcode")))
+	if err!=nil{
+		fmt.Println("Token creation failed")
+		return;
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"token":tokenString,
+	})
+}
+
+
+func register(c *gin.Context){
+	var body struct {
+		Username string
+		Password string
+	}
+	if c.Bind(&body) != nil{
+		fmt.Println("Cannot bind the data");
+		return
+	}
+
+	hash , error := bcrypt.GenerateFromPassword([]byte(body.Password),10)
+	if error != nil{
+		fmt.Println("Cannot generate Hash Value");
+		return
+	}
+
+	user := User{Username:body.Username,Password:string(hash)}
+	result := DB.Create(&user)
+
+	if result.Error != nil{
+		fmt.Println("Cannot store in the database");
+		return
+	}
+	fmt.Println("Succesfully Inserted")
+	c.JSON(http.StatusOK, gin.H{})
 }
